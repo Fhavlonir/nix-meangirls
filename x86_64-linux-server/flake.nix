2aeb7b55538e673b9299e48e9142a99e372c969a
Philip Johansson <philip.johansson@synotio.se>
2026-03-26 17:41:20 +0100

    Changed to nixos-25.11

--- x86_64-linux-server/flake.nix
+++ x86_64-linux-server/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     nvf.url = "github:notashelf/nvf/v0.8";
