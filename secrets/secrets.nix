a299354e1df67bb1d2a94f269d1a2a35abb55e69
Philip Johansson <philip.johansson@synotio.se>
2026-03-30 08:35:11 +0200

    moved secret owner conf to correct file

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,6 @@ let
   philip = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLcdbypsebwiqsfbHCoMOW7Trj8StZ8yByDTGTk4s6F";
 in {
   "molly_vapid_privkey_env.age".publicKeys = [pvgj philip];
-  "ldap_root_pw.age" = {
-    publicKeys = [pvgj philip];
-    owner = "portunus";
-  };
-  "ldap_user_pw.age" = {
-    publicKeys = [pvgj philip];
-    owner = "portunus";
-  };
+  "ldap_root_pw.age".publicKeys = [pvgj philip];
+  "ldap_user_pw.age".publicKeys = [pvgj philip];
 }
