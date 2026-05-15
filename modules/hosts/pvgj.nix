a16eed8446faec0490439699c464e3f3ede40a78
Philip Johansson <filip.johansson1996@gmail.com>
2026-05-15 18:40:45 +0200

    Fixes for pvgj server

--- modules/hosts/pvgj.nix
+++ modules/hosts/pvgj.nix
@@ -17,13 +17,19 @@
   ];
 
   pvgjInline = {
-    networking.hostName = "pvgj";
-    networking.domain = "se";
+    networking = {
+      hostName = "pvgj";
+      domain = "se";
+    };
+    boot = {
+      initrd.availableKernelModules = ["ata_piix" "ahci" "vmw_pvscsi" "sd_mod" "sr_mod"];
+      initrd.kernelModules = [];
 
-    boot.initrd.availableKernelModules = ["ata_piix" "ahci" "vmw_pvscsi" "sd_mod" "sr_mod"];
-    boot.initrd.kernelModules = [];
-    boot.kernelModules = [];
-    boot.extraModulePackages = [];
+      kernelModules = [];
+      extraModulePackages = [];
+      loader.grub.enable = false;
+      loader.systemd-boot.enable = true;
+    };
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/ca62a5da-087c-4ef5-8c49-c3b02a2bf677";
