9b93677be5f50ce16bc5a4f2305468f01ee88f0e
Philip Johansson <filip.johansson1996@gmail.com>
2026-05-15 20:41:47 +0200

    sudo nopasswd on nixos

--- modules/platform/nixos.nix
+++ modules/platform/nixos.nix
@@ -8,5 +8,16 @@ in {
   flake.modules.nixos.desktop = _: {
     system.stateVersion = "25.11";
     time.timeZone = "Europe/Stockholm";
+    security.sudo.extraRules = [
+      {
+        users = [vars.userName];
+        commands = [
+          {
+            command = "ALL";
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
   };
 }
