1ba0bd460e3d2fd1d72441c72a196ed58540ae8c
Philip Johansson <filip.johansson1996@gmail.com>
2026-05-15 22:42:45 +0200

    pvgj network conf

--- modules/hosts/pvgj.nix
+++ modules/hosts/pvgj.nix
@@ -20,6 +20,45 @@
     networking = {
       hostName = "pvgj";
       domain = "se";
+      firewall.allowedTCPPorts = [22 80 443];
+      enableIPv6 = true;
+      useNetworkd = true;
+      nameservers = ["10.24.112.2" "10.24.112.3"];
+      interfaces.ens192 = {
+        ipv4.addresses = [
+          {
+            address = "10.24.168.7";
+            prefixLength = 24;
+          }
+        ];
+        ipv4.routes = [
+          {
+            address = "10.24.112.0";
+            prefixLength = 24;
+            via = "10.24.168.1";
+          }
+        ];
+      };
+      interfaces.ens224 = {
+        ipv4.routes = [
+          {
+            address = "10.0.0.0";
+            prefixLength = 8;
+            via = "10.24.68.1";
+          }
+        ];
+        ipv4.addresses = [
+          {
+            address = "10.24.68.7";
+            prefixLength = 24;
+          }
+        ];
+      };
+      defaultGateway = {
+        address = "10.24.168.1";
+        interface = "ens192";
+      };
+      useDHCP = false;
     };
     boot = {
       initrd.availableKernelModules = ["ata_piix" "ahci" "vmw_pvscsi" "sd_mod" "sr_mod"];
