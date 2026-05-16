438c74985289ff5c43b293e4334294822ea523b5
Philip Johansson <filip.johansson1996@gmail.com>
2026-05-16 02:58:15 +0200

    More fishy changes

--- modules/platform/nixos.nix
+++ modules/platform/nixos.nix
@@ -8,6 +8,7 @@ in {
   flake.modules.nixos.desktop = _: {
     system.stateVersion = "25.11";
     time.timeZone = "Europe/Stockholm";
+    programs.fish.enable = true;
     security = {
       run0.enableSudoAlias = true;
       run0.wheelNeedsPassword = false;
